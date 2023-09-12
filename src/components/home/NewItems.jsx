4b2e3fdf7001f63ea3b525bc293604bdeeca1aa9
LaurenceLama <laurencelamafse@gmail.com>
2023-09-12 19:27:45 +0800

    Debugged not showing collection - pending countdown

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -25,7 +25,6 @@ export default function NewItems() {
 
   return (
     <section id="section-items" className="no-bottom">
-      {newItem.nftImage}
       <div className="container">
         <div className="row">
           <div className="col-lg-12">
@@ -34,7 +33,7 @@ export default function NewItems() {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {newItem.map((collection, index) => {
+          {newItem.map((collection, index) => (
             <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
               <div className="nft__item">
                 <div className="author_list_pp">
@@ -48,7 +47,8 @@ export default function NewItems() {
                     <i className="fa fa-check"></i>
                   </Link>
                 </div>
-                <div className="de_countdown">5h 30m 32s</div>
+                {/* needs dynamic countdown */}
+                <div className="de_countdown">5h 30m 32s</div> 
 
                 <div className="nft__item_wrap">
                   <div className="nft__item_extra">
@@ -69,7 +69,7 @@ export default function NewItems() {
                     </div>
                   </div>
 
-                  <Link to="/item-details">
+                  <Link to={`/item-details/${collection.nftId}`}>
                     <img
                       src={collection.nftImage}
                       className="lazy nft__item_preview"
@@ -78,69 +78,13 @@ export default function NewItems() {
                   </Link>
                 </div>
                 <div className="nft__item_info">
-                  <Link to="/item-details">
+                  <Link to={`/item-details/${collection.nftId}`}>
                     <h4>{collection.title}</h4>
                   </Link>
-                  <div className="nft__item_price">3.08 ETH</div>
+                  <div className="nft__item_price">{collection.price}</div>
                   <div className="nft__item_like">
                     <i className="fa fa-heart"></i>
-                    <span>69</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          })}
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-              <div className="nft__item">
-                <div className="author_list_pp">
-                  <Link
-                    to="/author"
-                    data-bs-toggle="tooltip"
-                    data-bs-placement="top"
-                    title="Creator: Monica Lucas"
-                  >
-                    <img className="lazy" src={AuthorImage} alt="" />
-                    <i className="fa fa-check"></i>
-                  </Link>
-                </div>
-                <div className="de_countdown">5h 30m 32s</div>
-
-                <div className="nft__item_wrap">
-                  <div className="nft__item_extra">
-                    <div className="nft__item_buttons">
-                      <button>Buy Now</button>
-                      <div className="nft__item_share">
-                        <h4>Share</h4>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-facebook fa-lg"></i>
-                        </a>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-twitter fa-lg"></i>
-                        </a>
-                        <a href="">
-                          <i className="fa fa-envelope fa-lg"></i>
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-
-                  <Link to="/item-details">
-                    <img
-                      src={nftImage}
-                      className="lazy nft__item_preview"
-                      alt=""
-                    />
-                  </Link>
-                </div>
-                <div className="nft__item_info">
-                  <Link to="/item-details">
-                    <h4>Pinky Ocean</h4>
-                  </Link>
-                  <div className="nft__item_price">3.08 ETH</div>
-                  <div className="nft__item_like">
-                    <i className="fa fa-heart"></i>
-                    <span>69</span>
+                    <span>{collection.likes}</span>
                   </div>
                 </div>
               </div>
